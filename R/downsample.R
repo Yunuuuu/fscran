7c374956322c29ee402132e0acf3b3eccfd41c2f
yun <yunyunpp96@outlook.com>
2024-06-18 19:35:04 +0800

    add function `downsample`

---
+++ R/downsample.R
@@ -0,0 +1,27 @@
+#' Downsample cells based on their neighbors
+#'
+#' @export
+downsample <- function(object, ...) UseMethod("downsample")
+
+#' @inheritParams runPCA
+#' @export
+#' @rdname downsample
+downsample.SingleCellExperiment <- function(object, ...,
+                                            dimred = "PCA", n_dimred = NULL,
+                                            exprs_values = NULL) {
+    mat <- .get_mat_from_sce(object, exprs_values, dimred, n_dimred)
+    downsample(object = mat, ...)
+}
+
+#' @inheritParams runPCA
+#' @inheritDotParams scran.chan::downsampleByNeighbors.chan -x -num.threads
+#' @seealso [downsampleByNeighbors.chan][scran.chan::downsampleByNeighbors.chan]
+#' @inherit scran.chan::downsampleByNeighbors.chan return
+#' @export
+#' @rdname downsample
+downsample.default <- function(object, ..., threads = 1L) {
+    scran.chan::downsampleByNeighbors.chan(
+        x = object, ...,
+        num.threads = threads
+    )
+}
