acd4fb460f2f3c30be43799a65d748214985b81d
yun <yunyunpp96@outlook.com>
2024-06-19 02:13:39 +0800

    add function `%||%`

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,5 @@
+`%||%` <- function(x, y) if (is.null(x)) y else x
+
 set_threads <- function(threads,
                         arg = rlang::caller_arg(threads),
                         call = rlang::caller_call()) {
