e7dc943329146b1a07a6b78e63175973af97aaca
yun <yunyunpp96@outlook.com>
2024-06-19 15:01:35 +0800

    fix error: rebundant argument passed on to logNormCounts

--- R/logNormAndPCA.R
+++ R/logNormAndPCA.R
@@ -8,32 +8,12 @@
 #' @export
 logNormAndPCA <- function(object, ...) UseMethod("logNormAndPCA")
 
-#' @export
-#' @rdname logNormAndPCA
-logNormAndPCA.SingleCellExperiment <- function(object, ..., name = "PCA") {
-    pca <- NextMethod("logNormAndPCA", object = object, ...)
-    add_dimred_to_sce(object, pca, name)
-}
-
-#' @export
-#' @rdname logNormAndPCA
-logNormAndPCA.Seurat <- function(object, ...,
-                                 assay = NULL, layer = "counts",
-                                 name = "PCA") {
-    pca <- NextMethod("logNormAndPCA",
-        object = object, ..., assay = assay, layer = layer
-    )
-    add_dimred_to_seurat(object, pca, name, assay, layer, NULL)
-}
-
-#' @inheritDotParams logNormCounts
-#' @export
-#' @rdname logNormAndPCA
-logNormAndPCA.default <- function(object, d = 50L, scale = FALSE, ...,
-                                  subset_row = NULL,
-                                  batch = NULL, batch_method = NULL,
-                                  threads = NULL) {
+.logNormAndPCA <- function(object, d = 50L, scale = FALSE, ...,
+                           subset_row = NULL,
+                           batch = NULL, batch_method = NULL,
+                           threads = NULL) {
     threads <- set_threads(threads)
+
     # nromalization, adjust for differences in sequencing depth --------
     logcounts <- logNormCounts(
         object = object,
@@ -53,3 +33,23 @@ logNormAndPCA.default <- function(object, d = 50L, scale = FALSE, ...,
         threads = threads
     )
 }
+
+#' @export
+#' @rdname logNormAndPCA
+logNormAndPCA.SingleCellExperiment <- function(object, ..., name = "PCA") {
+    pca <- .logNormAndPCA(object = object, ...)
+    add_dimred_to_sce(object, pca, name)
+}
+
+#' @export
+#' @rdname logNormAndPCA
+logNormAndPCA.Seurat <- function(object, ..., assay = NULL, layer = "counts",
+                                 name = "PCA") {
+    pca <- .logNormAndPCA(object = object, ..., assay = assay, layer = layer)
+    add_dimred_to_seurat(object, pca, name, assay, layer)
+}
+
+#' @inheritDotParams logNormCounts
+#' @export
+#' @rdname logNormAndPCA
+logNormAndPCA.default <- .logNormAndPCA
