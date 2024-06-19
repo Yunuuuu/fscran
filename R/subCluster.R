f7004d081d0acf9583b9656f2fa3ebcc28f1c2c8
yun <yunyunpp96@outlook.com>
2024-06-19 18:13:42 +0800

    check arguments before running

--- R/subCluster.R
+++ R/subCluster.R
@@ -34,21 +34,30 @@ subCluster.default <- function(object, groups, restricted = NULL, ...,
         new_level,
         c("insert", "fill-end", "fill-start", "start", "end")
     )
+    by_groups <- split(seq_along(groups), groups)
+    if (!is.null(restricted)) {
+        restricted <- as.character(restricted)
+        missing <- setdiff(restricted, names(by_groups))
+        if (length(missing)) {
+            cli::cli_abort(c(
+                "Invalid value found in {.arg restricted}",
+                i = "Cannot find {.val {missing}} group{?s}"
+            ))
+        }
+        by_groups <- .subset(by_groups, restricted)
+    }
+    # Do subclustering for each group ------------------------
     all_groups <- as.character(groups)
     all_levels <- levels(groups) %||% sort(unique(all_groups))
     all_attributes <- NULL
-    by_groups <- split(seq_along(groups), groups)
-    if (!is.null(restricted)) {
-        by_groups <- .subset2(by_groups, as.character(restricted))
-    }
-    # Do subclustering for specific groups
     for (group in names(by_groups)) {
         index <- .subset2(by_groups, group)
         sub_membership <- clusterFn(object = object[, index], ...)
         all_groups[index] <- sprintf(format, group, sub_membership)
+        # deal with the new-generated levels ---------------------
         new_levels <- sprintf(format, group, levels(sub_membership))
         all_levels <- order_levels(policy, group, all_levels, new_levels)
-        # save the attributes returned by `clusterFn`
+        # save the attributes returned by `clusterFn` ------------
         sub_attributes <- attributes(sub_membership)
         names(sub_attributes) <- sprintf(
             "subCluster%s_%s", group, names(sub_attributes)
